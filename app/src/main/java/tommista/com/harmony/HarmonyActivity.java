22317827911d26aa0c45f4a16050a6a2cadc0ae0
Josh Beridon <jberidon@gmail.com>
2015-02-28 17:13:57 -0600

    Having troubles with streaming

--- app/src/main/java/tommista/com/harmony/HarmonyActivity.java
+++ app/src/main/java/tommista/com/harmony/HarmonyActivity.java
@@ -2,44 +2,56 @@ package tommista.com.harmony;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
 
-public class HarmonyActivity extends ActionBarActivity {
 
+public class HarmonyActivity extends ActionBarActivity implements MediaPlayer.OnPreparedListener {
+    private  MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_harmony);
-        try {
-            Uri uri = Uri.parse("http://soundcloud.com/misterwives/reflections-reflections-ep");
-            //http://api.soundcloud.com/tracks/7399237/stream?client_id=55de8cc1d6246dd72e0a78b1c70fd91a
-            MediaPlayer mediaPlayer = MediaPlayer.create(this.getApplicationContext(), uri);
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            Log.i("Before","This is before the try catch");
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-            Log.i("Play","App should be playing music");
-            Log.i("Error","Could not get the url");
-            Log.i("After","This is after the try catch");
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setOnPreparedListener(this);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try{
+            mMediaPlayer.setDataSource("https://api.soundcloud.com/tracks/7399237/stream?client_id=55de8cc1d6246dd72e0a78b1c70fd91a");
+            Log.i("URLLLL", "should have set url");
+
+            //"http://api.soundcloud.com/tracks/7399237/stream?client_id=XXX"
+            mMediaPlayer.prepare();
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+            Log.i("playing", "illegalargument exception");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("playing", "ioexception");
+
+
         }
-        catch(Exception ex){
+
 
         }
-
-
-
-
-
-
-
-
+    public void onPrepared(MediaPlayer mediaPlayer){
+        mMediaPlayer.start();
+        Log.i("playing", "should be playing the song");
     }
+
+
+
+
+
+
+
+
 
 
     @Override
