1036789b00ec89c53b375d9d595b663778d9e666
Tommy Brown <thomasbrown73095@gmail.com>
2015-02-28 15:59:40 -0500

    vcr drawables

--- app/src/main/java/tommista/com/harmony/ui/VCRView.java
+++ app/src/main/java/tommista/com/harmony/ui/VCRView.java
@@ -2,7 +2,11 @@ package tommista.com.harmony.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+
+import tommista.com.harmony.R;
 
 /**
  * Created by tbrown on 2/28/15.
@@ -10,6 +14,10 @@ import android.widget.LinearLayout;
 public class VCRView extends LinearLayout{
 
     private Context context;
+
+    private Button rewindButton;
+    private Button playButton;
+    private Button forwardButton;
 
     public VCRView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -19,6 +27,31 @@ public class VCRView extends LinearLayout{
     @Override
     protected void onFinishInflate(){
         super.onFinishInflate();
+
+        rewindButton = (Button) this.findViewById(R.id.rewind_button);
+        playButton = (Button) this.findViewById(R.id.play_button);
+        forwardButton = (Button) this.findViewById(R.id.forward_button);
+
+        rewindButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        playButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        forwardButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
