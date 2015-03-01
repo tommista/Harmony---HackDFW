8ae5f48c1bc61dbe37f6d99dd979cc94d1db8968
Tommy Brown <thomasbrown73095@gmail.com>
2015-02-28 20:19:19 -0500

    more ui changes

--- app/src/main/java/tommista/com/harmony/TrackPlayer.java
+++ app/src/main/java/tommista/com/harmony/TrackPlayer.java
@@ -1,11 +1,16 @@
 package tommista.com.harmony;
 
+import tommista.com.harmony.managers.PlaylistManager;
+import tommista.com.harmony.models.Track;
+
 /**
  * Created by tbrown on 2/28/15.
  */
 public class TrackPlayer {
 
-    public static TrackPlayer instance;
+    private static TrackPlayer instance;
+
+    private PlaylistManager playlistManager;
 
     public static TrackPlayer getInstance(){
         if(instance == null){
@@ -15,7 +20,11 @@ public class TrackPlayer {
     }
 
     private TrackPlayer(){
+        playlistManager = PlaylistManager.getInstance();
+    }
 
+    public Track getCurrentTrack(){
+        return playlistManager.trackList.get(0);
     }
 
     public void playPauseTrack(){
