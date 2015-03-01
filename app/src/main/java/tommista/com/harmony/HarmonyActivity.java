f54419ce04cd73c7eeb6ecb379aca93dd2f61a35
Jacob Lubecki <jacoblubecki@gmail.com>
2015-02-28 18:27:03 -0600

    core spotify functionality

--- app/src/main/java/tommista/com/harmony/HarmonyActivity.java
+++ app/src/main/java/tommista/com/harmony/HarmonyActivity.java
@@ -1,19 +1,41 @@
 package tommista.com.harmony;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import tommista.com.harmony.spotify.SpotifyAuthenticator;
+import tommista.com.harmony.spotify.SpotifyPlayer;
 
 
 public class HarmonyActivity extends ActionBarActivity {
+
+    private static final int SPOTIFY_REQUEST_CODE = 1337;
+
+    boolean playing = true;
+
+    private SpotifyPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_harmony);
+
+        SpotifyAuthenticator.authenticate(this, SPOTIFY_REQUEST_CODE);
+
+        player = new SpotifyPlayer(this, "2TpxZ7JUBn3uw46aR7qd6V");
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SPOTIFY_REQUEST_CODE) {
+            SpotifyAuthenticator.handleResponse(this, resultCode, intent);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +57,18 @@ public class HarmonyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeState(View view) {
+        if(playing) {
+            player.pause();
+            ((Button)view).setText("Play");
+        }
+        else {
+            player.resume();
+            ((Button)view).setText("Pause");
+        }
+
+        playing = !playing;
     }
 }
