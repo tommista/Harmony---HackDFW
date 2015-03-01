a728813de06139e1470bdd00173ced52b0de5fb8
Tommy Brown <thomasbrown73095@gmail.com>
2015-02-28 23:26:24 -0500

    Added a SpotifyTrack constructor to Track

--- app/src/main/java/tommista/com/harmony/models/Track.java
+++ app/src/main/java/tommista/com/harmony/models/Track.java
@@ -1,5 +1,7 @@
 package tommista.com.harmony.models;
 
+import tommista.com.harmony.spotify.models.SpotifyTrack;
+
 /**
  * Created by tbrown on 2/28/15.
  */
@@ -9,6 +11,12 @@ public class Track {
         this.title = title;
         this.artist = artist;
         this.imageURL = "http://imgs.xkcd.com/comics/dress_color.png";
+    }
+
+    public Track(SpotifyTrack spotifyTrack){
+        this.title = spotifyTrack.name;
+        this.artist = spotifyTrack.artists.get(0).name;
+        this.imageURL = spotifyTrack.album.images.get(0).url;
     }
 
     public String title;
